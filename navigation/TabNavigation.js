9907670246026b8e9f427e1c757865350681fe53
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-20 15:21:43 -0400

    clean up

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
-import AuthScreen from './screens/AuthScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
+import React , { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+import AuthScreen from '../screens/AuthScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import MapScreen from '../screens/MapScreen';
+import DeckScreen from '../screens/DeckScreen';
+import ReviewScreen from '../screens/ReviewScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const stcknavigator = createStackNavigator({
+  review: { screen: ReviewScreen},
+  settings: { screen: SettingsScreen}
+})
+
+const  subScreen = createBottomTabNavigator({
+  map: { screen: MapScreen },
+  deck: { screen: DeckScreen },
+  review: {
+    screen: stcknavigator
+  }
+})
+const TabNavigators = createBottomTabNavigator({
+  welcome: { screen: WelcomeScreen},
+  auth: { screen: AuthScreen},
+  main: subScreen
+});
 
 
-const TabNavigator = createBottomTabNavigator({
-    welcome: { screen: WelcomeScreen},
-    auth: { screen: AuthScreen}
-  });
-  
-  export default createAppContainer(TabNavigator);
+
+
+//const MainNavigator = createAppContainer(TabNavigators);
+export default TabNavigators;

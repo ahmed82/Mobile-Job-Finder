792f833e4197aa04189aeb282258acd00430b46f
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-20 15:21:16 -0400

    insert some slide data in the Welcome Screen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Slides from '../components/Slides';
 
+const SLIDE_DATA = [
+    { text: 'Welcome to JobApp'},
+    { text: 'Use this to get a jop'},
+    { text: 'Set Your location, then swipe away'},
+];
 
 class WelcomeScreen extends Component {
     render() {
         return(
             <View>
-                <Text>WelcomeScreen</Text>
+                <Slides data ={SLIDE_DATA} />
             </View>
         );
     }

b8316170aa886a77c54abf73de38474acd30acba
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-20 15:19:14 -0400

    create new Slides.js file in new folder named components

---
+++ components/Slides.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class Slides extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>{data}</Text>
+            </View>
+        )
+    }
+}
+
+export default Slides;

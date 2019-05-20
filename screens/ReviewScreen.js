893995cc4fb03f0635ac11f7d34c363fa6a51f4d
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-20 15:17:33 -0400

    fix the navegate in the new React Navigation 3.0

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { Button } from 'react-native-elements';
 
 
 class ReviewScreen extends Component {
 
-     static navigationOptions = { 
+     static navigationOptions = (obj) => { 
+         const { navigation } = obj;
+         console.log(JSON.stringify(obj, null, 2));
+         return {
         title: 'Review Jobs',
-       // headerRight:  <Button title="Settings" onPress={ () => navigation ('settings')} /> 
-       header: ({ navigate }) => {   
-        return { 
-         // right: <Button title="Settings" onPress={ () => {}} />
-         headerRight: <Text>Go Right</Text>
-        };}
+        headerRight:  <Button 
+            title="Settings"
+            onPress={ () => navigation.navigate('settings')} 
+            type="clear"
+            color="rgba(0, 122, 255, 1)"
+            
+            /> 
+  
        /*  header: ({ navigate }) => {  // old reactnavigation not in version 3.0
             return { 
               right: <Button title="Settings" onPress={ () => {}} />
                 right: <Text>Go Right</Text>
             };
         } */ 
-
+     }
     }
 
 

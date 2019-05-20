a577cc3286f1bed5c4640f5f355eb3524874e5f5
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-20 00:57:56 -0400

    add react-native-elements to use the Buttom instead of the Text tag

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'reavt-native-elements';
 
 
 class ReviewScreen extends Component {
 
     static navigationOptions = { 
         title: 'Review Jobs' ,
-        headerRight: <Text>Go Right</Text>
+        headerRight: <Buttom title="Settins" onPress={ () => } />
        /*  header: () => {  // old reactnavigation not in version 3.0
             return { 
                 right: <Text>Go Right</Text>

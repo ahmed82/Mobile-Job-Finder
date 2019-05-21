ca1d6d9898b6373fb184fa46269d3f868c093b71
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-21 12:48:43 -0400

    add the facebook login for login and hundel the token inside the application and the user device

---
+++ actions/auth_actions.js
@@ -0,0 +1,43 @@
+
+import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
+import {
+    FACEBOOK_LOGIN_SUCCESS,
+    FACEBOOK_LOGIN_FAIL
+} from './types';
+
+// AsyncStorage.setItem('fb_token')
+/* export const facebookLogin = () => {
+    return function(dispatch) {
+        let token = await AsyncStorage.getItem('fb_token');
+        if (toked) {
+            //Dispath an action : Fb Login is done
+        } else {
+            // start up FB Login process
+        }
+    }
+}; that section beeing refactor in the next section */
+
+export const facebookLogin = () => dispatch => {
+        let token = await AsyncStorage.getItem('fb_token');
+        if (toked) {
+            //Dispath an action : Fb Login is done
+            dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+        } else {
+            // start up FB Login process
+            doFacebookLogin(dispatch);
+        }
+    };
+
+   const doFacebookLogin = async(dispatch) => {
+       let { type, token} = await Facebook.logInWithReadPermissionsAsync('2380414738686574', {
+           permissions: ['public_profile']
+       });
+
+       if (type === 'cancel') {
+           return dispatch ({ type: FACEBOOK_LOGIN_FAIL })
+       }
+       await AsyncStorage.setItem('fb_token',token);
+       dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+        
+    };

2c2608c22c6a8b5d2711fcd331938268b8239989
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-21 00:22:00 -0400

    add button on the last page of the tutorials to be used for facebook login authintication

--- components/Slides.js
+++ components/Slides.js
@@ -4,13 +4,17 @@ import { View, Text, ScrollView, Dimensions } from 'react-native';
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Slides extends Component {
+    renderLastSlide(index){
+         if (index === this.props.data.length -1)
+    }
+
     renderSlides(){
-        return this.props.data.map((slide) => {
-            console.log("---------------------------------------"+slide.data)
+        return this.props.data.map((slide, index) => { 
             return (
                 <View style={[styles.slideStyle,{backgroundColor:slide.color}]} 
                       key={slide.text}>
                     <Text style={styles.textStyle}>{slide.text}</Text>
+                    {this.renderLastSlide(index)}
                 </View>
             )
         })

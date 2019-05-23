f7c5cd4b46115194bc7beb2edcf8aae8a94cc7dc
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-23 12:32:46 -0400

    inhansement map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 
 
 class MapScreen extends Component {
+    state = {
+        mapLoaded: false,
+        initialRegion: {
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,}
+    }
+
+    componentDidMount() {
+        this.setState({ mapLoaded: true});
+    }
     render() {
+        if(! this.state.mapLoaded) {
+            <View style = {{ flex: 1, justifyContent: 'center'}}>
+                <ActivityIndicator size="larg" />
+            </View>
+
+        }
         return(
             <MapView
                 style={{ flex: 1 }}
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                    }}
-                />
+                /* initialRegion={{ */
+               Region = {this.state.initialRegion}
+               />
         );
     }
 
